1d63528dd8fd997f772cd4db96bdd419ae16781d
DebugWith-Lilly <lillyntombizonkenkosi@gmail.com>
2026-08-02 21:45:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function updateTime() {
+    const now = new Date();
+
+    const time = now.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit"
+    });
+
+    document.getElementById("time").textContent = time;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
